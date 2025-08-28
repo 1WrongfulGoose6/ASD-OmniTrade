cde5a602fca10764aaae9619b099a8fa5ef174de
Ali B <30826345+PrinceAlii@users.noreply.github.com>
2025-08-28 14:22:35 +1000

    hotfix/tests

--- src/__tests__/basic.test.js
+++ src/__tests__/basic.test.js
@@ -8,9 +8,9 @@ describe('Home component', () => {
     // If the home page renders without crashing, pass.
   });
 
-  it('displays OmniTrade text', () => {
+  it('renders a "View Portfolio" link', () => {
     render(<Home />);
-    expect(screen.getByText('OmniTrade')).toBeInTheDocument();
-    // Check to see that the 'OmniTrade' title is rendered
+    expect(screen.getByRole('link', { name: /View Portfolio/i })).toBeInTheDocument();
+    // Check to see that the "View Portfolio" link is rendered
   });
 });

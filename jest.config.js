0b4c976055ae86ebba4d18db043b98a8d806e101
Ali B <30826345+PrinceAlii@users.noreply.github.com>
2025-08-28 13:45:47 +1000

    fix jest configs

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ const { getJestProjects } = require('@nx/jest');
 
 module.exports = {
   projects: getJestProjects(),
-  // Let's tell Jest to use babel.config.jest.js for transforming files
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { configFile: './babel.config.jest.js' }],
   },
